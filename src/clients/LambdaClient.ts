11777e398efc9facad34682442d13b22ce1911a0
Bruno Dzikowski <dzikowski.bruno@gmail.com>
2025-03-18 18:07:00 +0100

    Fine tune lambda

--- src/clients/LambdaClient.ts
+++ src/clients/LambdaClient.ts
@@ -11,10 +11,11 @@ import {
 import * as AwsUtils from "../utils/AwsUtils.js";
 
 const LAMBDA_REGION = "eu-central-1";
-const LOGGING_ROLE_ARN =
-  "arn:aws:iam::314146339425:role/LambdaBasicExecutionRole";
+const AWS_ROLE_ARN = "arn:aws:iam::314146339425:role/LambdaBasicExecutionRole";
 const SOURCE_CODE_S3_BUCKET =
   "module-bucket-a60555b5-a452-46d5-8a9f-5248d2dc41a5";
+const TIMEOUT_MINUTES = 3;
+const MEMORY_MB = 1024;
 
 const client = new LambdaClient({
   credentials: AwsUtils.getCredentialProvider(),
@@ -42,11 +43,13 @@ export async function createLambda(
     Publish: true,
     Layers: [layerArn],
     FunctionName: lambdaName,
-    Role: LOGGING_ROLE_ARN,
+    Role: AWS_ROLE_ARN,
     Architectures: [Architecture.x86_64],
     Handler: "lambda_function.lambda_handler",
     PackageType: PackageType.Zip,
     Runtime: Runtime.python311,
+    MemorySize: MEMORY_MB,
+    Timeout: TIMEOUT_MINUTES,
   });
 
   const { FunctionArn } = await client.send(command);
